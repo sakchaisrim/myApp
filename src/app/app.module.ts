fa56f88b7b2d6054bedc4557b99032c7b20de6b2
=sakchaisrim <=sakchaisrim@gmail.com>
2022-06-03 21:30:31 +0700

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,43 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+// เรียกใช้งาน Angular Router
+import {RouterModule} from '@angular/router';
+
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    AboutComponent,
+    ContactComponent,
+    FooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      [
+        {
+          path: "",
+          component: HomeComponent
+        },
+        {
+          path: "about",
+          component: AboutComponent
+        },
+        {
+          path: "contact",
+          component: ContactComponent
+        }
+      ]
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
